cff8965551395b06723e79a39ebf4fb46554fcb9
Dean <Dean@OWNER>
2017-03-15 14:27:46 +0000

    Added a player display box and 2 lists (Filter and players)

--- AssignmentPart1/AssignmentPart1/MainWindow.xaml.cs
+++ AssignmentPart1/AssignmentPart1/MainWindow.xaml.cs
@@ -21,6 +21,8 @@ namespace AssignmentPart1
     public partial class MainWindow : Window
     {
         public List<Team> teamDisplay = new List<Team>();
+        public List<Player> PlayerDisplay = new List<Player>();
+        public List<Player> FilteredPlayerDisplay = new List<Player>();
 
         public MainWindow()
         {
@@ -35,12 +37,23 @@ namespace AssignmentPart1
             Team t3 = new Team() { TeamName = "Live", GoalDifference = 89, Points = 34 };
             Team t4 = new Team() { TeamName = "Arse", GoalDifference = 420, Points = 987234 };
 
+            Player p1 = new Player() { PlayerSurname = "Gerrard", PlayerNationality = "English", ShirtNumber = "9", TeamName = "Live" };
+            Player p2 = new Player() { PlayerSurname = "Ibrah", PlayerNationality = "Sweedish", ShirtNumber = "7", TeamName = "ManU" };
+            Player p3 = new Player() { PlayerSurname = "Sanchez", PlayerNationality = "Chilean", ShirtNumber = "7", TeamName = "Arse" };
+            Player p4 = new Player() { PlayerSurname = "Coutinho", PlayerNationality = "Brazilean", ShirtNumber = "8", TeamName = "Heart" };
+
             teamDisplay.Add(t1);
             teamDisplay.Add(t2);
             teamDisplay.Add(t3);
             teamDisplay.Add(t4);
 
+            PlayerDisplay.Add(p1);
+            PlayerDisplay.Add(p2);
+            PlayerDisplay.Add(p3);
+            PlayerDisplay.Add(p4);
+
             lbxDisplay.ItemsSource = teamDisplay;
+            listBox_Players.ItemsSource = PlayerDisplay;
         }
 
         private void btnAddTeam_Click(object sender, RoutedEventArgs e)
@@ -50,6 +63,26 @@ namespace AssignmentPart1
             teamDisplay.Add(new Team() { TeamName = tbxTeamName.Text, GoalDifference = int.Parse(tbxGoalDifference.Text), Points = int.Parse(tbxClubPoints.Text) });
 
             lbxDisplay.ItemsSource = teamDisplay;
+        }
+
+        private void lbxDisplay_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Team selectedTeam = lbxDisplay.SelectedItem as Team;
+            FilteredPlayerDisplay.Clear();
+
+            foreach (var FiltPlayer in PlayerDisplay)
+            {
+                // Player s1 = new Player() { PlayerSurname = FiltPlayer.PlayerSurname, PlayerNationality = FiltPlayer.PlayerNationality, ShirtNumber = FiltPlayer.ShirtNumber, TeamName = FiltPlayer.TeamName };
+
+                if (selectedTeam.TeamName == FiltPlayer.TeamName)
+                {
+                    FilteredPlayerDisplay.Add(new Player() { PlayerSurname = FiltPlayer.PlayerSurname, PlayerNationality = FiltPlayer.PlayerNationality, ShirtNumber = FiltPlayer.ShirtNumber, TeamName = FiltPlayer.TeamName });
+                }
+            }
+            listBox_Players.ItemsSource = "";
+            listBox_Players.ItemsSource = FilteredPlayerDisplay;
+
+
         }
     }
 }
