bf40a259955d28c41d6184a413cc81c097c693f8
Dean <Dean@OWNER>
2017-03-19 21:34:03 +0000

    I hope this works ta fuck

---
+++ AssignmentPart1/AssignmentPart1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AssignmentPart1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Team> teamDisplay = new List<Team>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Team t1 = new Team() { TeamName = "Heart", GoalDifference = 9, Points = 6 };
+            Team t2 = new Team() { TeamName = "ManU", GoalDifference = 4, Points = 2435 };
+            Team t3 = new Team() { TeamName = "Live", GoalDifference = 89, Points = 34 };
+            Team t4 = new Team() { TeamName = "Arse", GoalDifference = 420, Points = 987234 };
+
+            teamDisplay.Add(t1);
+            teamDisplay.Add(t2);
+            teamDisplay.Add(t3);
+            teamDisplay.Add(t4);
+
+            lbxDisplay.ItemsSource = teamDisplay;
+        }
+
+        private void btnAddTeam_Click(object sender, RoutedEventArgs e)
+        {
+            lbxDisplay.ItemsSource = "";
+
+            teamDisplay.Add(new Team() { TeamName = tbxTeamName.Text, GoalDifference = int.Parse(tbxGoalDifference.Text), Points = int.Parse(tbxClubPoints.Text) });
+
+            lbxDisplay.ItemsSource = teamDisplay;
+        }
+    }
+}
